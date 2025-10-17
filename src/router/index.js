02ad7a32c54adbcb6359fb12b4c99bba7c9be82c
SherryPing0857 <sherry564100@gmail.com>
2025-10-17 17:39:26 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import IntroView from "../views/IntroView.vue";
 import FaqView from "../views/FaqView.vue";
 import TrainingView from "../views/TrainingView.vue";
@@ -7,7 +7,8 @@ import News from "../views/NewsView.vue";
 import LoginView from "../views/LoginView.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // 這裡改用 hash 模式
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
