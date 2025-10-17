76bfddfcabc69d4af7358d4a9cffe62d293f6bca
SherryPing0857 <sherry564100@gmail.com>
2025-10-17 22:17:36 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import IntroView from "../views/IntroView.vue";
 import FaqView from "../views/FaqView.vue";
 import TrainingView from "../views/TrainingView.vue";
@@ -7,38 +7,14 @@ import NewsView from "../views/NewsView.vue";
 import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),  // <-- 改成 hash 模式
   routes: [
-        {
-      path: "/",
-      name: "login",
-      component: LoginView,
-    },
-    {
-      path: "/news",
-      name: "news",
-      component: NewsView,
-    },
-    {
-      path: "/faq",
-      name: "faq",
-      component: FaqView,
-    },
-    {
-      path: "/training",
-      name: "training",
-      component: TrainingView,
-    },
-    {
-      path: "/forms",
-      name: "forms",
-      component: FormsView,
-    },
-    {
-      path: "/intro",
-      name: "intro",
-      component: IntroView,
-    },
+    { path: "/", name: "login", component: LoginView },
+    { path: "/news", name: "news", component: NewsView },
+    { path: "/faq", name: "faq", component: FaqView },
+    { path: "/training", name: "training", component: TrainingView },
+    { path: "/forms", name: "forms", component: FormsView },
+    { path: "/intro", name: "intro", component: IntroView },
   ],
 });
 
