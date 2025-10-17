784686839612a1d19b01dc63626308fc58095ba9
SherryPing0857 <sherry564100@gmail.com>
2025-10-17 22:58:02 +0800

    package

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import IntroView from "../views/IntroView.vue";
 import FaqView from "../views/FaqView.vue";
 import TrainingView from "../views/TrainingView.vue";
@@ -7,14 +7,38 @@ import NewsView from "../views/NewsView.vue";
 import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),  // <-- 改成 hash 模式
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/", name: "login", component: LoginView },
-    { path: "/news", name: "news", component: NewsView },
-    { path: "/faq", name: "faq", component: FaqView },
-    { path: "/training", name: "training", component: TrainingView },
-    { path: "/forms", name: "forms", component: FormsView },
-    { path: "/intro", name: "intro", component: IntroView },
+        {
+      path: "/",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/news",
+      name: "news",
+      component: NewsView,
+    },
+    {
+      path: "/faq",
+      name: "faq",
+      component: FaqView,
+    },
+    {
+      path: "/training",
+      name: "training",
+      component: TrainingView,
+    },
+    {
+      path: "/forms",
+      name: "forms",
+      component: FormsView,
+    },
+    {
+      path: "/intro",
+      name: "intro",
+      component: IntroView,
+    },
   ],
 });
 
