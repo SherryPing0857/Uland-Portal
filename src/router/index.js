debea4ad022a2749f76df100d208c7a8db016927
SherryPing0857 <sherry564100@gmail.com>
2025-10-17 20:17:50 +0800

    delete

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,21 @@ import IntroView from "../views/IntroView.vue";
 import FaqView from "../views/FaqView.vue";
 import TrainingView from "../views/TrainingView.vue";
 import FormsView from "../views/FormsView.vue";
-import News from "../views/NewsView.vue";
+import NewsView from "../views/NewsView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+        {
+      path: "/",
+      name: "login",
+      component: LoginView,
+    },
     {
       path: "/news",
       name: "news",
-      component: News,
+      component: NewsView,
     },
     {
       path: "/faq",
